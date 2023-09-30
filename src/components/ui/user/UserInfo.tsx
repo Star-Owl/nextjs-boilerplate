a85f3af9b3f6a8c8239ce0a6f71132b2355824aa
Star-Owl <hasiradoo@starowl.dev>
2023-09-30 15:26:19 +0200

    UserInfo fixes

--- src/components/ui/user/UserInfo.tsx
+++ src/components/ui/user/UserInfo.tsx
@@ -11,15 +11,19 @@ interface Props {
 
 const UserInfo: FunctionComponent<Props> = ({ size = 'small' }) => {
 	const isLarge = size === 'large'
-	const typographyStyle = isLarge ? 'text-xxl' : 'text-base'
+	const typographyStyle = isLarge
+		? 'text-xxl'
+		: 'text-base cursor-pointer hover:underline'
 	const chipTextStyle = isLarge ? 'text-white' : 'text-white/[.60]'
-	const chipBgStyle = isLarge ? 'bg-black/[.3]' : 'bg-white/[.06]'
+	const chipBgStyle = isLarge
+		? 'bg-black/[.3]'
+		: 'bg-white/[.06] cursor-pointer'
 
 	return (
 		<React.Fragment>
 			<Twemoji options={{ className: 'emoji' }}>
 				<span
-					className={`w-max cursor-pointer overflow-hidden text-ellipsis whitespace-pre ${typographyStyle} font-bold hover:underline`}
+					className={`w-max overflow-hidden text-ellipsis whitespace-pre ${typographyStyle} font-bold`}
 				>
 					Hasira 🥃🪴
 				</span>
@@ -27,7 +31,7 @@ const UserInfo: FunctionComponent<Props> = ({ size = 'small' }) => {
 			<Chip
 				radius='sm'
 				classNames={{
-					base: `group ${chipBgStyle} backdrop-blur-md py-[.281rem] px-[.562rem] !h-auto rounded-[.375rem] cursor-pointer transition-colors hover:bg-white/[.12]`,
+					base: `group ${chipBgStyle} backdrop-blur-md py-[.281rem] px-[.562rem] !h-auto rounded-[.375rem] transition-colors hover:bg-white/[.12]`,
 					content: `${chipTextStyle} text-xs p-0 group-hover:text-white overflow-hidden text-ellipsis whitespace-pre leading-tight`,
 				}}
 			>
