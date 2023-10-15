c5dad94b9563b41542f2d39e7c4586b5956deba5
Star-Owl <hasiradoo@starowl.dev>
2023-10-15 22:27:32 +0200

    fix cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,14 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './filters/all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Authorization',
-  });
+  app.enableCors();
+  app.useGlobalFilters(new AllExceptionsFilter());
 
   await app.listen(3000);
 }
