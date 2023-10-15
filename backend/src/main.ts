bf4cd684816dab2bdedf4fb3c278874e64af5d1f
Star-Owl <hasiradoo@starowl.dev>
2023-10-15 22:00:41 +0200

    fix CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*',
+    origin: 'https://starowl.social',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
   });
